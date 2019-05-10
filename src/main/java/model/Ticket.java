9f4b339fd5b7be4ebe2063f612f7377525903f41
Ming He <mihe@thoughtworks.com>
2019-05-10 12:56:48 +0800

    refactor : move class to packages

---
+++ src/main/java/model/Ticket.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Ticket {
+    private Car car;
+
+    public Car getCar() {
+        return this.car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
